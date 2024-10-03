cc8c8db22b352c28b480820921892c20ef2df2b8
s-sacaprarulo <1042113@lwsd.org>
2024-10-03 13:44:19 -0700

    attempting to make title of tkinter window to be timer

--- text.py
+++ text.py
@@ -31,26 +31,30 @@ game_time = PersonalTimer(3)
 
 
 def move_window(root:tk):
-    window_width = 200
-    window_height = 150
-    screen_x = 1920 - window_width - 500
-    screen_y = 1200 - window_height - 400
+    window_width = 500
+    window_height = 350
+    screen_x = 2100 - window_width - 700
+    screen_y = 1400 - window_height - 400
     root.geometry(f"+{random.randrange(0, screen_x)}+{random.randrange(0, screen_y)}")
     game_time.update_flag_time()
 
 def open_window():
+    #setting up the window
     root = tk.Tk()
-    # Set the initial position of the window (e.g., 100x100)
+    root.title("VIRUS")
     root.geometry("400x300+100+100")
-    
+    root.wm_attributes("-topmost", 1)
 
     # Add a button to trigger window movement
     move_button = tk.Button(root, text="EXTEND YOUR PC'S LIVE BY 3 SECONDS", command=lambda: move_window(root))
     move_button.pack(pady=20)
 
-    # Bind the WM_DELETE_WINDOW event to call the on_closing function
-    # root.protocol("WM_DELETE_WINDOW", on_closing)
+
+    root.after(10, lambda:update_timer(root))
     root.mainloop()
+
+def update_timer(root:tk):
+    root.title(game_time.flag_time + 3 - game_time.curr_time)
 
 # def on_closing():
 #    print("CLOSED WINDOW")
@@ -60,30 +64,30 @@ window_thread = threading.Thread(target=open_window)
 window_thread.start()
 
 lost = False
-
+no_restart = True
 
 while not lost:
     game_time.update_curr_time()
     if (game_time.is_over_time()):
         lost = True
         break
+    time.sleep(0.01)
 
 if lost:
     messagebox.askokcancel("Virus", "you suck")
-
 
     result = messagebox.askyesno("Virus", "YOU HAVE LOST THE GAME! we would restart the computer now, but the debug option is on so we wont. Do you still want to restart it?")
     if (result):
         result = messagebox.askyesno("Virus", "ARE YOU SURE? this will restart your computer and any unsaved datat will be lost!")
         if (result):
-            print("RESTART")
             pygame.mixer.init()
             sound = pygame.mixer.Sound("C:/Users/1042113/Desktop/Cloned Repositories/ForFun/soundeffectLIB/Metal pipe falling sound effect(loud).mp3")
             sound.play()
-            pygame.time.delay(3000)
-            # playsound("C:/Users/1042113/Desktop/Cloned Repositories/ForFun/soundeffectLIB/Metal pipe falling sound effect(loud).mp3")
-            # restarts the computer if uncommented
-            # os.system("shutdown /r /t 0")
+            # pygame.time.delay(1200)
+            time.sleep(1.2)
+            print("RESTART")
+            if not no_restart:
+                os.system("shutdown /r /t 0")
 
 
 
