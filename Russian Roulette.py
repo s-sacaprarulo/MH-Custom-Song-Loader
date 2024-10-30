eb4dbfd53be9f5d6793a17c1d6cfb272f7e9bc6c
s-sacaprarulo <s-sacaprarulo@lwsd.org>
2024-10-30 11:45:37 -0700

    russian roulette

--- Russian Roulette.py
+++ Russian Roulette.py
@@ -9,9 +9,9 @@ import time
 import pygame
 
 
-def temp_function():
-    number = random.randint(0, 1)
-    if number == 0:
+def shot_fired(bullet_chambers:int):
+    number = random.randint(1, 6)
+    if number <= bullet_chambers:
         print("Lose")
         pygame.mixer.init()
         sound = pygame.mixer.Sound("C:/Users/1042113/Desktop/Cloned Repositories/ForFun/soundeffectLIB/Taco Bell Bong - Sound Effect (HD).mp3")
@@ -21,9 +21,9 @@ def temp_function():
         # os.system("shutdown /r /t 0") # shuts down the system if not commented
         output = messagebox.askyesno("Gambling", "You Suck")
         if output:
-            temp_function()
+            shot_fired(1)
 
-    elif number == 1:
+    else:
         print("Win")
         pygame.mixer.init()
         sound = pygame.mixer.Sound("C:/Users/1042113/Desktop/Cloned Repositories/ForFun/soundeffectLIB/onlymp3.to - vine_boom-_vBVGjFdwk4-192k-1708393618.mp3")
@@ -32,14 +32,14 @@ def temp_function():
         time.sleep(1.2)
         output = messagebox.askyesno("Gambling", "GG EZ")
         if output:
-            temp_function()
+            shot_fired(bullet_chambers +1)
 
 root = tk.Tk()
 root.title("Gamble")
 root.geometry("400x300+100+100")
 root.wm_attributes("-topmost", 1)
 
-button = tk.Button(root, text="Press at your own risk!", command=lambda: temp_function())
+button = tk.Button(root, text="Press at your own risk!", command=lambda: shot_fired(1))
 button.pack(pady=20)
 
 
