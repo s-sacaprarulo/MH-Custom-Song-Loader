7bf43bc70d3d830217d8c895119c0688542d7126
s-sacaprarulo <s-sacaprarulo@lwsd.org>
2024-10-30 10:18:42 -0700

    created new text file

---
+++ MoreFun.py
@@ -0,0 +1,16 @@
+import random
+from typing import List
+from typing import Dict
+import tkinter as tk
+from tkinter import messagebox
+import threading
+import os
+import time
+import pygame
+
+root = tk.Tk()
+root.title("Window")
+root.geometry("400x300+100+100")
+root.wm_attributes("-topmost", 1)
+
+root.mainloop()
